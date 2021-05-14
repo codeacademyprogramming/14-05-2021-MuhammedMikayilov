24fd938a15910d1760a979d9150054d8a0546ed5
Muhammed Mikayilov <muhammedmikayilov@Muhammed-MacBook-Pro.local>
2021-05-14 22:51:17 +0400

    Added index.js

---
+++ Homework/src/index.js
@@ -0,0 +1,6 @@
+import { content } from "./layouts/app";
+import { numberToWord } from "@muhammed.mikayilov/homework-day-09/js/app.js";
+
+import "./assets/scss/style.scss";
+
+console.log(numberToWord(1));
